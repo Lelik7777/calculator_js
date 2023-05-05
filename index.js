de60d272de1dc0b2652dc24e2e9d48439aaf460f
Lelik Jan <21alex7777@gmail.com>
2023-05-05 09:46:49 +0300

    feat: get data about buttons from dom

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
-console.log("hello world");
-console.log(getElement('[data-operation]'));
-console.log(getElement('[data-equals]',false));
+//? class for calculator
+class Calculator {
+  constructor(previousOperandElement, currentOperandElement) {
+    this.previousOperandElement = previousOperandElement;
+    this.currentOperandElement = currentOperandElement;
+    this.clear();
+  }
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = null;
+  }
 
+  delete() {}
+
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
+
+  chooseOperation() {}
+
+  compute() {}
+
+  updateDisplay() {
+    this.currentOperandElement.textContent = this.currentOperand;
+    this.previousOperandElement.textContent = this.previousOperand;
+  }
+
+  getDisplayNumber() {}
+}
+
+//? DOM elements
+//output
+const previousOperandElement = getElement("[data-previous-operand]", false);
+const currentOperandElement = getElement("[data-current-operand]", false);
+//buttons
+const numberButtons = getElement("[data-number]");
+const operationButtons = getElement("[data-operation]");
+const equalsButton = getElement("[data-equals]", false);
+const clearButton = getElement("[data-all-clear]", false);
+const deleteButton = getElement("[data-delete]", false);
+
+const calculator = new Calculator(
+  previousOperandElement,
+  currentOperandElement
+);
+//* events
+numberButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    calculator.appendNumber(this.textContent);
+    calculator.updateDisplay();
+  });
+});
+
+//? utils
 function getElement(element, all = true) {
   return all
     ? document.querySelectorAll(element)
